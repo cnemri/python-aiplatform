6e623bf0a366aac281629071ed6f2cadf56f5bf0
Chouaieb Nemri <nemri@google.com>
2025-03-19 14:04:40 +0100

    from IPython.core.display import display deprecated. replacing with from IPython.display import display

--- google/cloud/aiplatform/utils/_ipython_utils.py
+++ google/cloud/aiplatform/utils/_ipython_utils.py
@@ -146,8 +146,7 @@ def display_link(text: str, url: str, icon: Optional[str] = "open_in_new") -> No
         </script>
     """
 
-    from IPython.core.display import display
-    from IPython.display import HTML
+    from IPython.display import display, HTML
 
     display(HTML(html))
 
